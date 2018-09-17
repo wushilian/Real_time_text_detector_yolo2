29f574da698ed71cbea9cc4e56508cb4d6d023c0
wuxiaolian <2501038982@qq.com>
2018-09-17 20:09:22 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+
+check_point='ckpt'
+
+img_height=400
+img_width=400
+img_channel=1
+
+num_anchors=5
+classes='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+num_classes=1
+
+Grid_h=25
+Grid_w=25
+
+lambda_coor=5
+lambda_obj=0.5
+
+lr=1e-3
+
+anchors=[[0.57273,0.677385],
+[1.87446,2.06253],
+[3.33843,5.47434],
+[7.88282,3.52778],
+[9.77052,9.16828]]
+
+prob_th = 0.8
+iou_th = 0.5
